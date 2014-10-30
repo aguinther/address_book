60249d78c83f28c36d0a9da000d9899f1bbacf61
Andy Guinther <andrew.guinther@gmail.com>
2014-10-29 18:18:42 -0600

    Updated ruby version and completed validations homework

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,13 @@
 class City < ActiveRecord::Base
+  validates :elevation,
+    numericality: {
+      greater_than_or_equal_to: 3315,
+      if: ->(city) { city.state == 'CO' },
+      message: 'is too low, lowest point in CO is 3315 feet.'
+    }
+
+  validates :state, length: { is: 2 }
+  validates :population, numericality: {greater_than_or_equal_to: 0}
+
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,10 @@
 class Person < ActiveRecord::Base
+  validates :first_name, :last_name, :email, :address_line_one, :city, :state, :zip_code, presence: true
+  validates :email, format: {
+    with: /.*@.*/
+  }
+  validates :zip_code, length: {
+    is: 5
+  }
+  validates :state, length: { is: 2 }
 end
